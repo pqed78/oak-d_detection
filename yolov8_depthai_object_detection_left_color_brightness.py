6f3a410e7588e3e31fe3971a3ca8b4c11d6dd877
Taek Yong Hwang <taekyong.hwang@gmail.com>
2024-07-02 15:33:46 +0900

    brightness dependent camera change included

---
+++ yolov8_depthai_object_detection_left_color_brightness.py
@@ -0,0 +1,116 @@
+from ultralytics import YOLO
+import cv2
+import math 
+import depthai as dai
+import time
+import numpy as np
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define source and output
+camRgb = pipeline.create(dai.node.ColorCamera)
+monoLeft = pipeline.create(dai.node.MonoCamera)
+# monoRight = pipeline.create(dai.node.MonoCamera)
+
+
+
+xoutVideo = pipeline.create(dai.node.XLinkOut)
+xoutLeft = pipeline.create(dai.node.XLinkOut)
+# xoutRight = pipeline.create(dai.node.XLinkOut)
+
+xoutVideo.setStreamName("video")
+xoutLeft.setStreamName("left")
+# xoutRight.setStreamName("right")
+
+# Properties
+camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.CAM_B)
+# monoRight.setBoardSocket(dai.CameraBoardSocket.CAM_C)
+
+
+# Linking
+camRgb.video.link(xoutVideo.input)
+monoLeft.out.link(xoutLeft.input)
+# monoRight.out.link(xoutRight.input)
+
+device= dai.Device(pipeline)
+video = device.getOutputQueue(name="video", maxSize=1, blocking=False)
+left = device.getOutputQueue(name="left", maxSize=1, blocking=False)
+# right = device.getOutputQueue(name="right", maxSize=1, blocking=False)
+
+
+
+# model
+model = YOLO("yolov8s.pt")
+
+# object classes
+classNames = ["person"]
+
+# object details
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+color = (255, 0, 0)
+thickness = 2
+
+while True:
+    videoIn = video.get()
+    img=videoIn.getCvFrame()
+    brightness_color=np.array(img).sum()
+    
+    results = model(img, stream=True)
+
+    leftIn = left.get()
+    imgl=leftIn.getCvFrame()
+    imgl0=cv2.cvtColor(imgl, cv2.COLOR_GRAY2BGR)
+    resultsl = model(imgl0, stream=True)
+    
+    # rightIn = right.get()
+    # imgr=rightIn.getCvFrame()
+    # imgr0=cv2.cvtColor(imgr, cv2.COLOR_GRAY2BGR)
+    # resultsr = model(imgr0, stream=True)
+
+    if brightness_color>1e8:
+        imgs=[(img, results) ]
+    else:
+        imgs=[(imgl, resultsl)]
+
+    for img, results in imgs:
+        # coordinates
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # class name
+                cls = int(box.cls[0])
+                if cls==0:
+                    # bounding box
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+                    org = [x1, y1]
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                    cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    
+    if brightness_color>1e8:
+        img_view=img
+    else:
+        img_view=imgl
+
+    cv2.imshow('Oak-D', img_view)
+
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+cv2.destroyAllWindows()
+
+# # Process results list
+# for result in results:
+#     boxes = result.boxes  # Boxes object for bounding box outputs
+#     masks = result.masks  # Masks object for segmentation masks outputs
+#     keypoints = result.keypoints  # Keypoints object for pose outputs
+#     probs = result.probs  # Probs object for classification outputs
+#     obb = result.obb  # Oriented boxes object for OBB outputs
+#     result.show()  # display to screen
+#     result.save(filename="result.jpg")  # save to disk
